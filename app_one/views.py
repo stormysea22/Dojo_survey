3ed0e4f0fe3d58699830da1d06e77ba26d35cffc
Becca Theismann <becca@theismannconsulting.com>
2021-02-17 12:47:38 -0500

    Added radios, drop menu, and styling. Project is complete.

--- app_one/views.py
+++ app_one/views.py
@@ -6,9 +6,13 @@ def index(request):
 def create_user(request):
     name_form = request.POST['name']
     email_form = request.POST['email']
+    language_form = request.POST['language']
+    location_form = request.POST['location']
+
     context = {
         "name_template" : name_form,
         "email_template" : email_form,
+        "language_template" : language_form,
+        "location_template" : location_form,
     }
     return render(request, 'result.html', context)
-
